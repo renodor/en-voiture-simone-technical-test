f855c9aadab3fa7801d47a488cbe53696ce8a10e
renaud dor <renaud.dor@gmail.com>
2022-12-08 17:44:27 +0100

    Add defaut scope ordering by time to potato price

--- app/models/potato_price.rb
+++ app/models/potato_price.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal:true
+
 class PotatoPrice < ApplicationRecord
   validates :price, :time, presence: true
   validates :time, uniqueness: true
+
+  default_scope { order(time: :asc) }
 
   def self.min_price_for_day(date_time)
     where(time: date_time.beginning_of_day..date_time.end_of_day).minimum(:price)
